ed7060ada7e42bd516643abd44b00581bae754a5
Michael Granados <michaelgranados@gmail.com>
2011-03-20 01:13:17 -0300

    Erros colocados em um ambiente separado

--- ARecord.php
+++ ARecord.php
@@ -115,25 +115,3 @@ class Modeler_ARecord
         return $form;
     }
 }
-
-class ARecord_Empty_Fields extends Exception
-{
-    public function __construct($message = NULL, $code = 1)
-    {
-        if (empty($message)) {
-            $message = 'You need to especify the \'$fields\' OR define \'$autofields\' = true propriety of object.';
-        }
-        parent::__construct($message, $code);
-    }
-}
-
-class ARecord_No_Primary extends Exception
-{
-    public function __construct($message = NULL, $code = 1)
-    {
-        if (empty($message)) {
-            $message = 'You need to especify the \'$primary\' OR define \'$autofields\' = true propriety of object.';
-        }
-        parent::__construct($message, $code);
-    }
-}
