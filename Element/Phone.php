aa3c8fa9a290b2579343afefc875d87cf7d351f7
Michael Granados <michaelgranados@gmail.com>
2011-05-18 00:29:29 -0300

    Recuperando os dados do telefone

--- Element/Phone.php
+++ Element/Phone.php
@@ -5,6 +5,9 @@ class Modeler_Element_Phone implements Modeler_Element
     public function render(Modeler_Formulator $formulator, array $values)
     {
         $fieldset  = '<fieldset class="telefone"><legend>Telefones</legend>';
+        if (!empty($values['telefone'])) {
+            $fieldset .= $this->renderValues($values['telefone']);
+        }
         $fieldset .= '<div class="telefone-wrap">';
         $fieldset .= '
                 <div class="telefone-wrap">
@@ -32,9 +35,27 @@ class Modeler_Element_Phone implements Modeler_Element
         $fieldset .= '</div>';
         $fieldset .= '</fieldset>';
         return $fieldset;
+    }
 
+    public function renderValues(array $values)
+    {
+        $fields = '';
+        foreach ($values as $key=>$item) {
+            $fields .= '
 
+                <label id="id_'.$key.'" class="first" style="display: block;">
+                    <span class="input-form-telefone">
+                        <input type="hidden" name="telefone[id_'.str_replace('id_', '', $key).'][tipo]" value="'.$item['tipo'].'" />
+                        <input type="hidden" name="telefone[id_'.str_replace('id_', '', $key).'][ddd]" value="'.$item['ddd'].'" />
+                        <input type="hidden" name="telefone[id_'.str_replace('id_', '', $key).'][telefone]" value="'.$item['telefone'].'" />
+                        <span class="text_tel">('.$item['ddd'].') '.$item['telefone'].'</span> 
+                        <button type="button" class="action edit ui-state-highlight">editar</button>
+                        <button type="button" class="action delete ui-state-error">remover</button>
+                    </span>
+                </label>
 
-
+            ';
+        }
+        return $fields;
     }
 }
