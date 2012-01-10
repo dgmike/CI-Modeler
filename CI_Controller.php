8e7c12ff0550d6f1ec1430f511e0fb93ce204a3f
Michael Granados <michaelgranados@gmail.com>
2012-01-10 02:16:41 -0200

    Adicionando um HOTFIX para as versões do CodeIgniter

---
+++ CI_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!class_exists('Controller')):
+
+/**
+ * FIX versions of CodeIgniter
+ **/
+class Controller extends CI_Controller
+{
+}
+
+endif;
--- Crudder.php
+++ Crudder.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'CI_Controller.php';
+
 class Modeler_Crudder extends Controller
 {
     public $model        = null;
